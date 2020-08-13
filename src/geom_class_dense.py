59d26217fc4c456086e54a50088ba3114e9e53dc
Leonardo <lzepeda@math.mit.edu>
2020-08-13 09:38:34 -0500

    bug: issue with cuDNN first solution

--- src/geom_class_dense.py
+++ src/geom_class_dense.py
@@ -3,6 +3,9 @@
 
 # I use the regular keras layuers for simplicity
 import tensorflow as tf 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
 from tensorflow import keras
 from tensorflow.keras import layers
 
